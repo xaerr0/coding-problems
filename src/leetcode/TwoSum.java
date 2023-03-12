a81bf5c3f2e73a33ba590e332358c8cb5f1419a5
Danny Brown <xaerr00@gmail.com>
2023-03-12 10:09:29 -0700

    Leetcode problems

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -19,7 +19,6 @@ package leetcode;
  Input: nums = [3,3], target = 6
  Output: [0,1]
 
-
  Constraints:
 
  2 <= nums.length <= 104
--- src/leetcode/ValidParentheses.java
+++ src/leetcode/ValidParentheses.java
@@ -11,7 +11,6 @@ import java.util.Stack;
  Open brackets must be closed in the correct order.
  Every close bracket has a corresponding open bracket of the same type.
 
-
  Example 1:
 
  Input: s = "()"
