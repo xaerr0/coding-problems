96b51f0cd485e3a83a6189f10a7e042fd696bcb9
Danny Brown <xaerr00@gmail.com>
2023-03-21 00:16:09 -0700

    LeetCode #7

---
+++ src/leetcode/_7_ReverseInteger.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+/**
+ * Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside
+ * the signed 32-bit integer range [-231, 231 - 1], then return 0.
+ *
+ * Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ *
+ *
+ * Example 1:
+ *
+ * Input: x = 123
+ * Output: 321
+ * Example 2:
+ *
+ * Input: x = -123
+ * Output: -321
+ * Example 3:
+ *
+ * Input: x = 120
+ * Output: 21
+ *
+ *
+ * Constraints:
+ *
+ * -231 <= x <= 231 - 1
+ */
+public class _7_ReverseInteger {
+    public static void main(String[] args) {
+
+        System.out.println(reverse(123));
+    }
+
+    public static int reverse(int x) {
+        x = 123;
+        int result = 0;
+
+        while (x != 0) {
+            int tail = x % 10;
+            int newResult = result * 10 + tail;
+            if ((newResult - tail) / 10 != result) {
+                return 0;
+            }
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+}
