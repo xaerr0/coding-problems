415c5b00bf868ba56c4342d3942162037b75bfd9
Danny Brown <xaerr00@gmail.com>
2023-03-23 22:38:03 -0700

    HackerRank - Birthday Cake Candles w/ Array Solution

--- src/hackerrank/BirthdayCakeCandles.java
+++ src/hackerrank/BirthdayCakeCandles.java
@@ -62,4 +62,20 @@ public class BirthdayCakeCandles {
         }
         return result;
     }
+
+    public static int birthdayCakeCandles2(int[] arr) {
+        int max = arr[0];
+        int count = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i ++) {
+            if (max == arr[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
